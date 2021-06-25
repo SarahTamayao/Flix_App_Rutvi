e4dc5c82a0f8e34a2d54c61bdc3cca41749e1d14
Rutvi Shah <rutvims@fb.com>
2021-06-25 13:18:26 -0400

    collection view finished

---
+++ Flix_App/View Controllers/MoviesGridViewController.m
@@ -0,0 +1,92 @@
+//
+//  MoviesGridViewController.m
+//  Flix_App
+//
+//  Created by rutvims on 6/25/21.
+//
+
+#import "MoviesGridViewController.h"
+#import "MovieCollectionCell.h"
+#import "UIImageView+AFNetworking.h"
+
+@interface MoviesGridViewController () <UICollectionViewDelegate, UICollectionViewDataSource>
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (nonatomic, strong) NSArray *movies;
+@end
+
+@implementation MoviesGridViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.collectionView.dataSource = self;
+    self.collectionView.delegate = self;
+    
+    [self fetchMovies];
+    
+    UICollectionViewFlowLayout *layout = (UICollectionViewFlowLayout *) self.collectionView.collectionViewLayout;
+    
+    layout.minimumInteritemSpacing = 5;
+    layout.minimumLineSpacing = 5;
+    
+    CGFloat postersPerLine = 2;
+    CGFloat itemWidth = (self.collectionView.frame.size.width - layout.minimumInteritemSpacing * (postersPerLine - 1))/postersPerLine;
+    CGFloat itemHeight = itemWidth * 1.5;
+    layout.itemSize = CGSizeMake(itemWidth, itemHeight);
+}
+
+-(void) fetchMovies{
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Cannot Get Movies" message:@"The internet connectino appears to be offline" preferredStyle:(UIAlertControllerStyleAlert)];
+    UIAlertAction *tryAgain = [UIAlertAction actionWithTitle:@"Try Again" style:(UIAlertActionStyleCancel) handler:^(UIAlertAction *_Nonnull action){
+        [self fetchMovies];
+    }];
+    [alert addAction:tryAgain];
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=bc576bd97242828b55b4c70fc10e4f3a&language=en-US&page=1"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error != nil) {
+            [self presentViewController:alert animated:YES completion: ^{}];
+            NSLog(@"%@", [error localizedDescription]);
+        }
+       else {
+           NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+           NSLog(@"%@", dataDictionary);
+
+           self.movies = dataDictionary[@"results"];
+           [self.collectionView reloadData];
+       }
+    }];
+    [task resume];
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (nonnull __kindof UICollectionViewCell *)collectionView:(nonnull UICollectionView *)collectionView cellForItemAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    MovieCollectionCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"MovieCollectionCell" forIndexPath:indexPath];
+    NSDictionary *movie = self.movies[indexPath.item];
+    
+    NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
+    
+    NSString *posterURLString = movie[@"poster_path"];
+    NSString *fullPosterURLString = [baseURLString stringByAppendingString:posterURLString];
+    
+    NSURL *posterURL = [NSURL URLWithString:fullPosterURLString];
+    
+    cell.posterView.image = nil;
+    [cell.posterView setImageWithURL:posterURL];
+    
+    return cell;
+}
+
+- (NSInteger)collectionView:(nonnull UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return self.movies.count;
+}
+
+@end
---
+++ Flix_App/Views/MovieCollectionCell.m
@@ -0,0 +1,12 @@
+//
+//  MovieCollectionCell.m
+//  Flix_App
+//
+//  Created by rutvims on 6/25/21.
+//
+
+#import "MovieCollectionCell.h"
+
+@implementation MovieCollectionCell
+
+@end
